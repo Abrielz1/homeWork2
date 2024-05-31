37241f385a9fc06d4ec253cae728761a16e155fe
Abriel <abrielz11@gmail.com>
2024-05-31 12:41:01 +0300

    2nd is doen

--- gateway/src/main/java/ru/skillbox/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/ru/skillbox/gateway/config/GatewayConfig.java
@@ -1,6 +1,8 @@
 package ru.skillbox.gateway.config;
 
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.skillbox.gateway.filter.CustomGlobalFilter;
@@ -11,4 +13,14 @@ public class GatewayConfig {
     GlobalFilter customGF() {
         return new CustomGlobalFilter();
     }
+
+//    @Bean
+//    RouteLocator customRoutes(RouteLocatorBuilder builder) {
+//        return builder
+//                .routes()
+//                .route("order-service",
+//                        r -> r.path("/skillbox/api/order/**")
+//                                .uri("lb://order-service")
+//                ).build();
+//    }
 }
