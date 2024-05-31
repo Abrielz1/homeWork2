ef7fbf472f7ce5de67f6baad1fc995247b7bd348
Abriel <abrielz11@gmail.com>
2024-05-31 10:51:12 +0300

    2nd is doen

--- gateway/src/main/java/ru/skillbox/gateway/filter/CustomGlobalFilter.java
+++ gateway/src/main/java/ru/skillbox/gateway/filter/CustomGlobalFilter.java
@@ -2,20 +2,16 @@ package ru.skillbox.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-public class CustomGlobalFilter implements GlobalFilter, Ordered {
+@Component
+public class CustomGlobalFilter implements GlobalFilter {
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("example of logic in custom filter: " + exchange.getRequest().getPath());
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
         return chain.filter(exchange);
-    }
-
-    @Override
-    public int getOrder() {
-        return 0;
     }
 }
